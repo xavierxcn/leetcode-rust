4181ee1ec96cdf57e2576303a2e040e183269cdb
xavier <xavier@xavierx.cn>
2024-07-07 23:33:19 +0800

    remove-duplicates-from-sorted-array.rs

---
+++ examples/remove-duplicates-from-sorted-array.rs
@@ -0,0 +1,30 @@
+
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/
+fn main() {
+
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.len() == 1 {
+            return 1
+        }
+        let mut i = 0;
+        let mut j = 0;
+        while j < nums.len() {
+            if i < j && nums[i] != nums[j] {
+                i += 1;
+                nums[i] = nums[j];
+            }
+
+            j += 1
+        }
+
+
+        (i + 1) as i32
+
+
+    }
+}

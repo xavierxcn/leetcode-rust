7b52354bb4cd0fa63cd6c539b4e0cc3a9a9167a6
xavier <xavier@xavierx.cn>
2024-07-16 22:44:54 +0800

    reverse-linked-list.rs

--- examples/reverse-linked-list.rs
+++ examples/reverse-linked-list.rs
@@ -1,3 +1,5 @@
+use std::ptr::null;
+
 fn main() {
 
 }
@@ -34,5 +36,18 @@ impl Solution {
         }
 
         prev
+    }
+
+    pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        head.and_then(|mut n| {
+            match n.next {
+                Some(mut m) => {
+                    n.next = Self::swap_pairs(m.next);
+                    m.next = Some(n);
+                    Some(m)
+                },
+                None => Some(n)
+            }
+        })
     }
 }
